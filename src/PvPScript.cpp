ba814085da093e85f998863fbb663e7c6f4b02bf
Lee <Talamortis@gmail.com>
2024-03-11 20:58:31 +0000

    Script update
    Updated Script

--- src/PvPScript.cpp
+++ src/PvPScript.cpp
@@ -19,7 +19,7 @@ class PvPScript : public PlayerScript
 public:
     PvPScript() : PlayerScript("PvPScript") {}
 
-    void OnPlayerKilledByCreature(Creature* killer, Player* killed)
+    void OnPlayerKilledByCreature(Creature* killer, Player* killed, bool& durabilityLoss)
     {
         if (!sConfigMgr->GetOption<bool>("PvPChest", true))
             return;
@@ -35,31 +35,11 @@ public:
             if (Pet* pet = killer->ToPet())
                 if (Player* owner = pet->GetOwner())
                 {
-                    if (owner->GetSession()->GetRemoteAddress() == killed->GetSession()->GetRemoteAddress())
-                        return;
-
-                    // Dont drop loot if killed is not worth honor for owner
-                    if (!owner->isHonorOrXPTarget(killed))
+                    if (!CheckConditions(owner, killed))
                         return;
                 }
 
-        // if player has Ress sickness do not spawn chest
-        if (killed->HasAura(15007))
-            return;
-
-        //Gurubashi Arena
-        if (killed->GetMapId() == 0 && killed->GetZoneId() == 33)
-            for (int i = 0; i < int(AreatoIgnore.size()); ++i)
-                if (killed->GetAreaId() == AreatoIgnore[i])
-                    return;
-
-
-        // Dont drop chess if player is in battleground
-        if (killed->GetMap()->IsBattlegroundOrArena())
-            return;
-
-        //Dont Drop chest if player is no worth XP
-        if (!killed->isHonorOrXPTarget(killer->GetOwner()))
+        if (!CheckConditions(nullptr, killed))
             return;
 
         // if target is killed and killer is pet
@@ -67,31 +47,10 @@ public:
         {
             if (GameObject* go = killer->SummonGameObject(SummonChest, killed->GetPositionX(), killed->GetPositionY(), killed->GetPositionZ(), killed->GetOrientation(), 0.0f, 0.0f, 0.0f, 0.0f, chestDespawn, false))
             {
-                switch (KillAnnounce)
-                {
-                case 1: //Announce in chat handler
-                    ChatHandler(killed->GetSession()).PSendSysMessage("You have been killed by player [%s] ", name.c_str());
-                    break;
-                case 2: //Announce in notifaction
-                    killed->GetSession()->SendNotification("You have been slain by [%s]", name.c_str());
-                    break;
-                case 3: // Announe in Notifaction and chathandler
-                    killed->GetSession()->SendNotification("You have been slain by [%s]", name.c_str());
-                    ChatHandler(killed->GetSession()).PSendSysMessage("You have been killed by player [%s] ", name.c_str());
-                    break;
-                }
-
+                AnnounceKill(KillAnnounce, killed, name.c_str());
                 killer->AddGameObject(go);
                 go->SetOwnerGUID(ObjectGuid::Empty); //This is so killed players can also loot the chest
-
-                for (int i = EQUIPMENT_SLOT_START; i < EQUIPMENT_SLOT_END; ++i)
-                    if (Item* pItem = killed->GetItemByPos(INVENTORY_SLOT_BAG_0, i))
-                    {
-                        uint8 slot = pItem->GetSlot();
-                        LootStoreItem storeItem = LootStoreItem(pItem->GetEntry(), 0, 100, 0, LOOT_MODE_DEFAULT, 0, 1, 1);
-                        go->loot.AddItem(storeItem);
-                        killed->DestroyItem(INVENTORY_SLOT_BAG_0, slot, true);
-                    }
+                SpawnChest(killed, go);
             }
         }
     }
@@ -103,64 +62,80 @@ public:
 
         std::string name = killer->GetName();
 
-        //if killer has same IP as death player do not drop loot as its cheating!
-        if (spawnchestIP)
-            if (killer->GetSession()->GetRemoteAddress() == killed->GetSession()->GetRemoteAddress())
-                return;
-
-        // if player has Ress sickness do not spawn chest
-        if (killed->HasAura(15007))
-            return;
-
-        // If player kills self do not drop loot
-        if (killer->GetGUID() == killed->GetGUID())
-            return;
-
-        //Gurubashi Arena
-        if (killed->GetMapId() == 0 && killed->GetZoneId() == 33)
-            for (int i = 0; i < int(AreatoIgnore.size()); ++i)
-                if (killed->GetAreaId() == AreatoIgnore[i])
-                    return;
-
-        // if killer not worth honnor do not drop loot
-        if (!killer->isHonorOrXPTarget(killed))
-            return;
-
-        // Dont drop chess if player is in battleground
-        if (killed->GetMap()->IsBattlegroundOrArena())
+        if (!CheckConditions(killer, killed))
             return;
 
         if (!killed->IsAlive())
         {
             if (GameObject* go = killer->SummonGameObject(SummonChest, killed->GetPositionX(), killed->GetPositionY(), killed->GetPositionZ(), killed->GetOrientation(), 0.0f, 0.0f, 0.0f, 0.0f, chestDespawn, false))
             {
-                switch (KillAnnounce)
-                {
-                case 1: //Announce in chat handler
-                    ChatHandler(killed->GetSession()).PSendSysMessage("You have been killed by player [%s] ", name.c_str());
-                    break;
-                case 2: //Announce in notifaction
-                    killed->GetSession()->SendNotification("You have been slain by [%s]", name.c_str());
-                    break;
-                case 3: // Announe in Notifaction and chathandler
-                    killed->GetSession()->SendNotification("You have been slain by [%s]", name.c_str());
-                    ChatHandler(killed->GetSession()).PSendSysMessage("You have been killed by player [%s] ", name.c_str());
-                    break;
-                }
-
+                AnnounceKill(KillAnnounce, killed, name.c_str());
                 killer->AddGameObject(go);
                 go->SetOwnerGUID(ObjectGuid::Empty); //This is so killed players can also loot the chest
-
-                for (uint8 i = EQUIPMENT_SLOT_START; i < EQUIPMENT_SLOT_END; ++i)
-                    if (Item* pItem = killed->GetItemByPos(INVENTORY_SLOT_BAG_0, i))
-                    {
-                        uint8 slot = pItem->GetSlot();
-                        LootStoreItem storeItem = LootStoreItem(pItem->GetEntry(), 0, 100, 0, LOOT_MODE_DEFAULT, 0, 1, 1);
-                        go->loot.AddItem(storeItem);
-                        killed->DestroyItem(INVENTORY_SLOT_BAG_0, slot, true);
-                    }
+                SpawnChest(killed, go);
             }
         }
+    }
+
+    bool CheckConditions(Player* killer, Player* killed)
+    {
+        //if killer has same IP as death player do not drop loot as its cheating!
+        if (spawnchestIP)
+            if (killer->GetSession()->GetRemoteAddress() == killed->GetSession()->GetRemoteAddress())
+                return false;
+
+        // if player has Ress sickness do not spawn chest
+        if (killed->HasAura(15007))
+            return false;
+
+        // If player kills self do not drop loot
+        if (killer->GetGUID() == killed->GetGUID())
+            return false;
+
+        // if killer not worth honnor do not drop loot
+        if (!killer->isHonorOrXPTarget(killed))
+            return false;
+
+        // Dont drop chess if player is in battleground
+        if (killed->GetMap()->IsBattlegroundOrArena())
+            return false;
+
+        //Gurubashi Arena
+        if (killed->GetMapId() == 0 && killed->GetZoneId() == 33)
+            for (int i = 0; i < int(AreatoIgnore.size()); ++i)
+                if (killed->GetAreaId() == AreatoIgnore[i])
+                    return false;
+
+        return true;
+    }
+
+    void AnnounceKill(uint32 phase, Player* killed, std::string name)
+    {
+        switch (phase)
+        {
+        case 1: //Announce in chat handler
+            ChatHandler(killed->GetSession()).PSendSysMessage("You have been killed by player [%s] ", name.c_str());
+            break;
+        case 2: //Announce in notifaction
+            killed->GetSession()->SendNotification("You have been slain by [%s]", name.c_str());
+            break;
+        case 3: // Announe in Notifaction and chathandler
+            killed->GetSession()->SendNotification("You have been slain by [%s]", name.c_str());
+            ChatHandler(killed->GetSession()).PSendSysMessage("You have been killed by player [%s] ", name.c_str());
+            break;
+        }
+    }
+
+    void SpawnChest(Player* killed, GameObject* go)
+    {
+        for (uint8 i = EQUIPMENT_SLOT_START; i < EQUIPMENT_SLOT_END; ++i)
+            if (Item* pItem = killed->GetItemByPos(INVENTORY_SLOT_BAG_0, i))
+            {
+                uint8 slot = pItem->GetSlot();
+                LootStoreItem storeItem = LootStoreItem(pItem->GetEntry(), 0, 100, 0, LOOT_MODE_DEFAULT, 0, 1, 1);
+                go->loot.AddItem(storeItem);
+                killed->DestroyItem(INVENTORY_SLOT_BAG_0, slot, true);
+            }
     }
 };
 
